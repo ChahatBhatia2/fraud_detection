17632857d283bb2c43621dceeb15d886e298863a
ChahatBhatia2 <bchahat2@gmail.com>
2022-04-24 17:46:15 +0530

    experiment

--- model.py
+++ model.py
@@ -9,4 +9,4 @@ def model2():
 def model3():
     print('adding code for ML model3')
 
-print('model 2 is performing better, will be using this')
+print('model 2 is performing better, will be using this'
